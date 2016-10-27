04d3fe79ac388a5a0ed325f3fb8d56d283656013
Piero Giusti <pierophp@gmail.com>
2016-10-27 15:30:45 -0200

    Fix template

--- src/resources/views/supervisor-generator.blade.php
+++ src/resources/views/supervisor-generator.blade.php
@@ -7,7 +7,7 @@ autorestart=true
 user={{config('queue_manager.supervisor_user')}}
 numprocs={{ $config->active ? ($config->max_instances ? $config->max_instances : 1) : 0 }}
 redirect_stderr=true
-stdout_logfile={{{{config('queue_manager.log_path')}}}}
+stdout_logfile={{config('queue_manager.log_path')}}
 stopwaitsecs=600
 stopsignal=INT
 
