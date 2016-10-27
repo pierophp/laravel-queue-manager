dde853b4f57f05b2f7dd2978623e54ef28541b60
Piero Giusti <pierophp@gmail.com>
2016-10-27 15:19:00 -0200

    Add config path to artisan and logs

--- src/config/config.php
+++ src/config/config.php
@@ -1,6 +1,8 @@
 <?php
 
 return [
+    'artisan_path' => base_path('artisan'),
+    'log_path' => storage_path('logs/worker.log'), 
     'supervisor_config_file' => '/etc/supervisor/conf.d/laravel-queue.conf',
     'supervisor_bin' => '/usr/bin/supervisorctl',
     'supervisor_user' => 'docker',
