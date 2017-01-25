6cdfdb210c0d5e3109e2442af7fc4b836fd6eba3
Piero Giusti <pierophp@gmail.com>
2017-01-25 11:18:07 -0200

    API Queue

--- src/config/config.php
+++ src/config/config.php
@@ -2,7 +2,9 @@
 
 return [
     'artisan_path' => base_path('artisan'),
-    'log_path' => storage_path('logs/worker.log'), 
+    'log_path' => storage_path('logs/worker.log'),
+    'execute_as_api' => false,
+    'api_url' => 'http://127.0.0.1/queue/process',
     'supervisor_config_file' => '/etc/supervisor/conf.d/laravel-queue.conf',
     'supervisor_bin' => '/usr/bin/supervisorctl',
     'supervisor_user' => 'docker',
