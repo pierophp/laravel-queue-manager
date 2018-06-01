87e8b1189d561b15b6aa5fb6c7e6e7d302cfc9a8
Piero Giusti <pierophp@gmail.com>
2018-06-01 09:52:52 -0300

    Fix schedulable

--- src/Core/Scheduler.php
+++ src/Core/Scheduler.php
@@ -16,44 +16,51 @@ class Scheduler
 
             $scheduleConfig = json_decode($schedulableQueue->schedule_config);
 
-            if (!isset($scheduleConfig->method)) {
-                event(new ScheduleError('Method not configured for ' . $schedulableQueue->name, ['command' => 'reread']));
-                continue;
-            }
+            try {
 
-            $method = $scheduleConfig->method;
-
-            $params = '';
-            if (isset($scheduleConfig->params)) {
-                $params = $scheduleConfig->params;
-            }
-
-            $schedule->call(function () use ($schedulableQueue, $scheduleConfig) {
-                $className = $schedulableQueue->class_name;
-
-                $lockKey = 'QUEUE_LOCK_' . $schedulableQueue->name . '_' . date('Y-m-d-H-i');
-
-                if (\Cache::has($lockKey)) {
-                    return;
+                if (!isset($scheduleConfig->method)) {
+                    event(new ScheduleError('ScheduleError - Method not configured for ' . $schedulableQueue->name));
+                    continue;
                 }
-                
-                $expiresAt = now()->addMinutes(10);
 
-                \Cache::put($lockKey, '1', $expiresAt);
-                
-                if ($scheduleConfig->props && is_array($scheduleConfig->props)) {
-                    foreach($scheduleConfig->props as $prop) {
+                $method = $scheduleConfig->method;
+
+                $params = '';
+                if (isset($scheduleConfig->params)) {
+                    $params = $scheduleConfig->params;
+                }
+
+                $schedule->call(function () use ($schedulableQueue, $scheduleConfig) {
+                    $className = $schedulableQueue->class_name;
+
+                    $lockKey = 'QUEUE_LOCK_' . $schedulableQueue->name . '_' . date('Y-m-d-H-i');
+
+                    if (\Cache::has($lockKey)) {
+                        return;
+                    }
+                    
+                    $expiresAt = now()->addMinutes(10);
+
+                    \Cache::put($lockKey, '1', $expiresAt);
+                    
+                    if ($scheduleConfig->props && is_array($scheduleConfig->props)) {
+                        foreach($scheduleConfig->props as $prop) {
+                            $job = (new $className());    
+                            $job->setName($schedulableQueue->name);
+                            $job->setProps($prop);
+                            $job->dispatch();
+                        }
+                    } else {
                         $job = (new $className());    
                         $job->setName($schedulableQueue->name);
-                        $job->setProps($prop);
                         $job->dispatch();
                     }
-                } else {
-                    $job = (new $className());    
-                    $job->setName($schedulableQueue->name);
-                    $job->dispatch();
-                }
-            })->$method($params);
+                })->$method($params);
+
+
+            } catch (\Throwable $e) {
+                event(new ScheduleError('Schedule Error - ' . $schedulableQueue->name . ' ' . $e->getMessage()));
+            }
         }
     }
 
